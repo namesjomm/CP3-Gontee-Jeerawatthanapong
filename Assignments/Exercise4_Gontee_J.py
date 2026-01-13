50a894c918663d992181f1cc1a6b3c9399546dfe
GONTEE.J <nmsjm.gontee@gmail.com>
2026-01-13 10:26:51 +0700

    Add files via upload

---
+++ Assignments/Exercise4_Gontee_J.py
@@ -0,0 +1,14 @@
+scr_1 = 85
+scr_2 = 90.5
+scr_3 = 93
+scr_4 = 100
+
+#output
+print("--- Your Score ---")
+
+print("Foundation English",":",scr_1)
+print("General Business",":",scr_2)
+print("Introduction to Computer Systems",":",scr_3)
+print("Computer Programming",":",scr_4)
+
+print("------------------")
