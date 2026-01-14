2f214be98bde84fbac5ebf5dc1331a43811c2d74
GONTEE.J <nmsjm.gontee@gmail.com>
2026-01-14 11:14:28 +0700

    Add files via upload

---
+++ Assignments/Exercise5_1_Gontee_J.py
@@ -0,0 +1,15 @@
+#Input
+firstNumber = int(input("Enter the first number: "))
+secondNumber = int(input("Enter the second number: "))
+
+#Process
+resultPlus = firstNumber + secondNumber
+resultMinus = firstNumber - secondNumber
+resultMultiplication = firstNumber * secondNumber
+resultDivision = firstNumber / secondNumber
+
+#Output
+print(firstNumber,"+",secondNumber,"=",resultPlus)
+print(firstNumber,"-",secondNumber,"=",resultMinus)
+print(firstNumber,"*",secondNumber,"=",resultMultiplication)
+print(firstNumber,"/",secondNumber,"=",resultDivision)
---
+++ Assignments/Exercise5_2_Gontee_J.py
@@ -0,0 +1,9 @@
+#Input
+distance = int(input("Enter the distance : "))
+time = int(input("Enter the time : "))
+
+#Process
+resultVelocity = distance/time
+
+#Output
+print(int(resultVelocity),"km/h")
